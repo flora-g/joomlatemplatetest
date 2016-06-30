c440b220c9fb96dadc93c725fb6121b4b38e3e25
Boris <crik-8@hotmail.Fr>
2016-06-30 14:23:00 +0200

    Update gulpfile.js
    Add ftp push, notification and Livereload

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,8 @@ var csso = require('gulp-csso');
 var cssimport = require("gulp-cssimport");
 var watch = require("gulp-watch");
 var livereload = require("gulp-livereload");
+var ftp = require('vinyl-ftp');
+var notify = require('gulp-notify');
 
 // création fichier production normal
 gulp.task('build', function() {
@@ -31,12 +33,31 @@ gulp.task('prod', function() {
           suffix: '.min'
         }))
         .pipe(gulp.dest('./css/'))
-		.pipe(livereload());
 });
 
+// envoi ftp
+var localFilesGlob = ['./css/**'];  // local folder
+var remoteFolder = '/wp-content/themes/mytheme/css'; //distant folder
+gulp.task( 'deploy', function () {
+
+    //ftp access
+    var conn = ftp.create( {
+        host:     'myhostaddress',
+        user:     'myusername',
+        password: 'mypassword'
+    } );
+
+
+    return gulp.src( localFilesGlob, { cwd: '.', buffer: false } )
+        .pipe( conn.newer( remoteFolder ) ) // only upload newer files
+        .pipe( conn.dest( remoteFolder ) )
+        .pipe(notify("Site mis à jour ! OH YEAH BABY :)")) // Notify what you want when file is uploaded
+        .pipe(livereload()); // Livereload your brother when file is uploded. You have to active the fonction in your brother with a plugin. For chrome use for exemple : https://chrome.google.com/webstore/detail/livereload/jnihajbhpnppcggbcgedagnkighmdlei?utm_source=plus
+} );
 
 //Watch task
-gulp.task('default',function() {
-	livereload.listen();   
-    gulp.watch('./sass/**/*.scss',['build', 'prod']); //watch change one scss and create two css file
+gulp.task('default',function() {   
+    gulp.watch('./src/**/*.scss',['build', 'prod']); //watch change one scss and create two css file
+    gulp.watch('./css/**/style.min.css', ['deploy']); //watch change one style.min.css and deploy on FTP
+    livereload.listen();
 });
